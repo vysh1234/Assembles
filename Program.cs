0e11819d25ad325aa2357be7763b6773bc925d33
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 11:55:09 +0530

    assembles

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using Assembles;
+
+namespace Assembles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementing Assemblies");
+        }
+            class Stringer
+        {
+            public void StringerMethod()
+            {
+                Console.WriteLine("This method is defined in assembles namespace under class stringer");
+
+                Stringer myStringInstance = new Stringer();
+                Console.WriteLine("client code executes");
+                myStringInstance.StringerMethod();
+            }
+        }
+        }
+    }
+
